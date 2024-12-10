4da8f294921a693830dffa401604200dccfc140c
shahzad50 <95250613+shahzad50@users.noreply.github.com>
2024-12-10 15:50:54 +0500

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,62 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Be0Xm5JfiU":
+        Script1();
+        break;
+      case "6bi0aN9OAb9":
+        Script2();
+        break;
+      case "5VOP1L4zQ6l":
+        Script3();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('6J73VNp7nF0');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6pP0nZFwfXs';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('5eOML9NGrQG');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6LJLJMmQeQ3';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
